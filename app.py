0f690b743f4b2ed3463de685c9e4bcb1d18df804
jakesh2 <143746054+jakesh2@users.noreply.github.com>
2024-11-05 08:50:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 # app.py
 import streamlit as st
+import streamlit.components.v1 as components
 from simulation_ui import build_simulation_ui
 from data_fitting_ui import build_data_fitting_ui
 
 # Mode selection toggle
 def main():
     mode = st.sidebar.radio("Select Mode", ["Simulation", "Data Fitting"])
-    
+    components.html("""
+    <head>
+    <meta name="google-site-verification"
+    content="uEdoedTxFYR7r8POe7yYULtC7y97ghmJjwew6XbpiUw" />
+    </head>
+    """,height=0)
     if mode == "Simulation":
         st.title("Epidemic Model Simulation")
         build_simulation_ui()
